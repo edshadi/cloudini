28e3a2761a2c6f6f652a156e508d1d505c8c9cab
Ed Shadi <edshadi@gmail.com>
2014-06-20 14:48:56 -0700

    turn the app into a chrome extension and inject it into gmail.

--- js/app.js
+++ js/app.js
@@ -5,4 +5,10 @@ var React = require('react');
 var Cloudini = require('./components/cloudini.react');
 var SidebarHeader = require('./components/sidebar-header.react');
 var data = require('./stores/data');
-React.renderComponent(<Cloudini header={<SidebarHeader fileStream="INBOX"/>} threadGroups={data.makeGroups()}/>, document.getElementById('container'));
+document.create
+var cloudiniSidebar = document.createElement('div');
+cloudiniSidebar.setAttribute('id', "cloudini");
+React.renderComponent(<Cloudini header={<SidebarHeader fileStream="INBOX"/>} threadGroups={data.makeGroups()}/>, cloudiniSidebar);
+window.onload = function() {
+  document.body.appendChild(cloudiniSidebar);
+}
